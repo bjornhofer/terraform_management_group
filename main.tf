bffddeb0cbcf3e05ac85949431c421c9a275333d
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-02-20 19:48:59 +0100

    added logic
    added logic to module to check for existence and consistence

--- main.tf
+++ main.tf
@@ -1,12 +1,58 @@
-provider "azurerm" {
-  features {}
-  alias = "management_group"
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
-} 
+// Gather data
+data "azurerm_management_group" "root" {
+  name = keys(var.mg_structure)[0]
+}
 
+data "azurerm_management_group" "all" {
+    for_each = toset(data.azurerm_management_group.root.all_management_group_ids)
+    name = split("/", each.key)[4]
+}
+
+// transform data
+locals {
+    mg_display_names = [for mg_detail in data.azurerm_management_group.all : mg_detail.display_name]
+    mg_parents = {for mg_detail in data.azurerm_management_group.all : mg_detail.display_name => split("/",mg_detail.parent_management_group_id)[4]}
+    mg_ids = {for mg_detail in data.azurerm_management_group.all : mg_detail.display_name => split("/",mg_detail.id)[4]}
+
+    test = element(var.mg_structure[keys(var.mg_structure)[0]][keys(var.mg_structure[keys(var.mg_structure)[0]])[0]], 0)
+
+    layer1_name = keys(var.mg_structure[keys(var.mg_structure)[0]])[0]
+    layer2_name = element(var.mg_structure[keys(var.mg_structure)[0]][keys(var.mg_structure[keys(var.mg_structure)[0]])[0]], 0)
+
+    layer1_id = local.mg_ids[local.layer1_name]
+    layer2_id = local.mg_ids[local.layer2_name]
+
+}
+
+// Check existence
+locals {
+    layer1_exists = contains(local.mg_display_names, local.layer1_name)
+    layer2_exists = contains(local.mg_display_names, local.layer2_name)
+}
+
+output "layer1_exists" {
+  value = local.layer1_exists
+}
+
+output "layer2_exists" {
+  value = local.layer2_exists
+}
+
+// Check parents
+locals {
+  layer1_parent_consitent = keys(var.mg_structure)[0] == local.mg_parents[local.layer1_name] ? true : false
+  layer2_parent_consitent = local.layer1_id == local.mg_parents[local.layer2_name] ? true : false
+}
+
+output "layer1_parent_consitent" {
+    value = local.layer1_parent_consitent
+}
+
+output "layer2_parent_consitent" {
+  value = local.layer2_parent_consitent
+}
+
+/*
 data "azurerm_management_group" "parent_mgmt_group" {
   display_name = var.mgmt_group_parent
   provider = azurerm.management_group
@@ -25,3 +71,4 @@ data "azurerm_management_group" "mgmt_group" {
   display_name = var.management_group_name
   provider = azurerm.management_group
 }
+*/
