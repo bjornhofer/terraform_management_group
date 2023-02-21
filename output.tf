bc55bd1c346d288e98c0a066619647d163398e11
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-02-21 12:01:52 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "management_group" {
-  value = var.dbg_simulate ? data.azurerm_management_group.mgmt_group : azurerm_management_group.mgmt_group
+  //value = var.dbg_simulate ? data.azurerm_management_group.mgmt_group : azurerm_management_group.mgmt_group
+  value = data.azurerm_management_group.mgmt_group
 }
