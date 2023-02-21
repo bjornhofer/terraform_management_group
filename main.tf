b5fd9167548fa6839b140fca6ccd639a2df394ab
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-02-21 11:58:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "azurerm_management_group" "parent_mgmt_group" {
   display_name = var.mgmt_group_parent
   provider = azurerm.management_group
 }
-
+/*
 resource "azurerm_management_group" "mgmt_group" {
   count                      = var.dbg_simulate ? 0 : 1
   parent_management_group_id = data.azurerm_management_group.parent_mgmt_group.id
@@ -19,6 +19,7 @@ resource "azurerm_management_group" "mgmt_group" {
   subscription_ids           = var.management_group_subscriptions
   provider = azurerm.management_group
 }
+*/
 
 data "azurerm_management_group" "mgmt_group" {
   count        = var.dbg_simulate ? 1 : 0
