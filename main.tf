538c4ed2390aa6464bb53cf8af0c5bd5f8a1d1a9
bjoernhoefer <bjoern@hoefer.or.at>
2023-05-03 18:54:07 +0000

    logic for 2 mgmt_grp layers

--- main.tf
+++ main.tf
@@ -7,22 +7,69 @@ provider "azurerm" {
   tenant_id = var.tenant_id
 } 
 
-data "azurerm_management_group" "parent_mgmt_group" {
+
+
+locals {
+  create__management_group = var.dbg_simulate ? false : var.management_group_create ? true : false
+  create__management_group_above_1 = var.management_group_above_1_name != null ? var.dbg_simulate ? false : var.management_group_above_1_create ? true : false : false
+  create__management_group_above_2 = var.management_group_above_2_name != null ? var.dbg_simulate ? false : var.management_group_above_2_create ? true : false : false
+}
+
+## Management Group
+data "azurerm_management_group" "mgmt_group_parent" {
   display_name = var.mgmt_group_parent
   provider = azurerm.management_group
 }
-/*
 resource "azurerm_management_group" "mgmt_group" {
-  count                      = var.dbg_simulate ? 0 : 1
-  parent_management_group_id = data.azurerm_management_group.parent_mgmt_group.id
+  count                      = local.create__management_group ? 1 : 0
+  parent_management_group_id = data.azurerm_management_group.mgmt_group_parent.id
   display_name               = var.management_group_name
   subscription_ids           = var.management_group_subscriptions
   provider = azurerm.management_group
 }
-*/
 
 data "azurerm_management_group" "mgmt_group" {
-  count        = var.dbg_simulate ? 1 : 0
+  count        = local.create__management_group ? 0 : 1
   display_name = var.management_group_name
+  provider = azurerm.management_group
+}
+
+## Management Group - One above
+data "azurerm_management_group" "parent_above_1" {
+  count = var.management_group_above_1_name != null ? 1 : 0
+  display_name = var.mgmt_group_parent
+  provider = azurerm.management_group
+}
+resource "azurerm_management_group" "above_1" {
+  count                      = local.create__management_group_above_1 ? 1 : 0
+  parent_management_group_id = data.azurerm_management_group.mgmt_group_parent_above_1.id
+  display_name               = var.management_group_above_1_name
+  subscription_ids           = var.management_group_above_1_subscriptions
+  provider = azurerm.management_group
+}
+
+data "azurerm_management_group" "above_1" {
+  count        = var.management_group_above_1_name != null ? local.create__management_group_above_1 ? 0 : 1 : 0
+  display_name = var.management_group_above_1_name
+  provider = azurerm.management_group
+}
+
+## Management Group - Two above
+data "azurerm_management_group" "parent_above_2" {
+  count = var.management_group_above_2_name != null ? 1 :0
+  display_name = var.management_group_above_2_parent
+  provider = azurerm.management_group
+}
+resource "azurerm_management_group" "above_2" {
+  count                      = local.create__management_group_above_2 ? 1 : 0
+  parent_management_group_id = data.azurerm_management_group.mgmt_group_parent_above_2.id
+  display_name               = var.management_group_above_2_name
+  subscription_ids           = var.management_group_above_2_subscriptions
+  provider = azurerm.management_group
+}
+
+data "azurerm_management_group" "above_2" {
+  count        = var.management_group_above_2_name != null ? local.create__management_group_above_2 ? 0 : 1 : 0
+  display_name = var.management_group_above_2_name
   provider = azurerm.management_group
 }
--- variable.tf
+++ variable.tf
@@ -14,8 +14,10 @@ variable "tenant_id" {
   type = string
 }
 
+## Management Group
 variable "mgmt_group_parent" {
   type = string
+  default = "Tenant Root Group"
 }
 
 variable "management_group_name" {
@@ -23,9 +25,65 @@ variable "management_group_name" {
   description = "Display name of the management group"
 }
 
+variable "management_group_create" {
+  type = bool
+  description = "Create the management group"
+  default = false
+}
+
 variable "management_group_subscriptions" {
   type        = list(any)
   description = "List of subscriptions housed in the mangement group"
+}
+
+## Management Group - one layer above
+variable "management_group_above_1_name" {
+  type = string
+  description = "Display name of the management group one layer above"
+  default = null
+}
+
+variable "management_group_above_1_create" {
+  type = bool
+  description = "Create the management group one layer above"
+  default = false
+}
+
+variable "management_group_above_1_subscriptions" {
+  type        = list(any)
+  description = "List of subscriptions housed in the mangement group one layer above"
+  default = null
+}
+
+variable "management_group_above_1_parent" {
+  type = bool
+  description = "Parent management group of the management group one layer above"
+  default = false
+}
+
+## Management Group - two layer above
+variable "management_group_above_2_name" {
+  type = string
+  description = "Display name of the management group two layer above"
+  default = null
+}
+
+variable "management_group_above_2_create" {
+  type = bool
+  description = "Create the management group two layer above"
+  default = false
+}
+
+variable "management_group_above_2_subscriptions" {
+  type        = list(any)
+  description = "List of subscriptions housed in the mangement group two layer above"
+  default = null
+}
+
+variable "management_group_above_2_parent" {
+  type = bool
+  description = "Parent management group of the management group two layer above"
+  default = false
 }
 
 variable "dbg_simulate" {
